1c88cb2dee747834b2f902f842cd9cfc194e6746
Abhishek Mangla <amangla@berkeley.edu>
2016-04-24 13:55:25 -0700

    update on ui

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def index
+    @posts = Post.all
+    if params[:search]
+      @posts = Post.search(params[:search]).order("created_at DESC")
+    else
+      @posts = Post.all.order('created_at DESC')
+    end
+  end
+  
+
   def like_post
     @post = Post.find(params[:id])
     @post.like += 1
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
 	validates :sport, :title, :text, presence: true
 	belongs_to :user
 	has_many :comments
+
+	def self.search(search)
+	  where("sport LIKE ?", "%#{search}%") 
+	  where("title LIKE ?", "%#{search}%")
+	end
 end
